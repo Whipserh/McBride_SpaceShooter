add7b16475d9e0043e97e1e512aeb4fd6aafb8a0
Whipserh <mcbrider@sheridancollege.ca>
2024-09-20 17:26:07 -0400

    finished class
    made player move based on accelertaion
    draw constellation in gizmo

--- SpaceShooter/Assets/Scripts/Controllers/Player.cs
+++ SpaceShooter/Assets/Scripts/Controllers/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using Codice.Client.BaseCommands.CheckIn.Progress;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,10 +9,76 @@ public class Player : MonoBehaviour
     public Transform enemyTransform;
     public GameObject bombPrefab;
     public Transform bombsTransform;
+    public float speed = 0.001f, accleration = 1f;
 
+    public float targetSpeed = 3;
+    public float timeToTargetSpeed = 2f;
+
+
+    private void Start()
+    {
+        accleration = targetSpeed / timeToTargetSpeed;
+
+        List<string> words = new List<string>();
+        words.Add("Dog");
+        words.Add("Cat");
+        words.Add("Log");
+        words.Insert(1, "Rat");
+
+        Debug.Log("The cat is at index: " +words.IndexOf("Cat"));
+    }
+
+
+
+    private Vector3 velocity;// = Vector3.right * 0.001f;
     void Update()
     {
-
+        
+        PlayerMovement();
     }
+
+
+    public void PlayerMovement()
+    {
+        //reset the volicity at the start of every frame
+        //NOTE: if we remove this we now have drag/momentum in our game
+        //velocity = Vector3.zero;
+
+
+        //NOTE: this is updating the player's velcoity based on the players acceleration
+        Vector3 newVelocity = velocity;
+        
+        
+        //hold left key
+        if (Input.GetKey(KeyCode.LeftArrow)){
+            newVelocity += Vector3.left * accleration * Time.deltaTime;
+
+        //hold down right velocity
+        } 
+        if (Input.GetKey(KeyCode.RightArrow)){
+            newVelocity += Vector3.right * accleration * Time.deltaTime;
+
+        }
+        if (Input.GetKey(KeyCode.UpArrow)){
+            newVelocity += Vector3.up * accleration * Time.deltaTime;
+
+        }
+        if (Input.GetKey(KeyCode.DownArrow)){
+            newVelocity += Vector3.down * accleration * Time.deltaTime;
+        }
+
+        if(newVelocity.magnitude <= targetSpeed)
+        {
+            velocity = newVelocity;
+        }
+
+        //NOTE: this is updating the player's position based on its speed
+        //velocity.Normalize();
+        //velocity *= speed;
+        transform.position += velocity * Time.deltaTime;
+        
+    }
+
+    
 
 }
--- SpaceShooter/Assets/Scripts/Controllers/Stars.cs
+++ SpaceShooter/Assets/Scripts/Controllers/Stars.cs
@@ -1,14 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class Stars : MonoBehaviour
 {
     public List<Transform> starTransforms;
     public float drawingTime;
+    private float timer = 0;
 
     // Update is called once per frame
     void Update()
     {
+        timer += Time.deltaTime;
+        DrawConstellation();
+    }
+
+
+    public void DrawConstellation()
+    {
+        /**
+        for (int i = 0; i < starTransforms.Count - 1; i++)
+        {
+            //draw line between each position
+            Debug.DrawLine(starTransforms[i].position, starTransforms[i + 1].position);
+        }
+        */
+
+        //current index
+        int index = Mathf.FloorToInt(timer / drawingTime) % starTransforms.Count;
+        Vector3 direction = starTransforms[index].position - starTransforms[(index+1)%starTransforms.Count].position;
+        float directLength = direction.magnitude;//grab the length of the direction
+        direction.Normalize();
+       Debug.DrawLine(starTransforms[index].position, starTransforms[index].position - direction*directLength*((timer%drawingTime)/drawingTime));
     }
 }
