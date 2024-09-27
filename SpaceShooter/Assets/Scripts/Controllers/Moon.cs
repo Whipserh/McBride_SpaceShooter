1966b3e084a7b7d66bdee2ce69a22f1f4167d6e1
Whipserh <mcbrider@sheridancollege.ca>
2024-09-27 17:16:11 -0400

    Task 3 Completed
    Made a moon orbit around a planet using vector

--- SpaceShooter/Assets/Scripts/Controllers/Moon.cs
+++ SpaceShooter/Assets/Scripts/Controllers/Moon.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Moon : MonoBehaviour
 {
@@ -15,6 +16,21 @@ public class Moon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        OrbitalMotion(1, 2, orbitedPlanet);
+    }
 
+    public Transform orbitedPlanet;
+    public void OrbitalMotion(float radius, float speed, Transform target)
+    {
+        if ((transform.position - target.position).magnitude != radius) {
+            transform.position = target.position + radius * Vector3.Normalize(transform.position - target.position);
+        }
+        //gives us a unit vector based around the origin
+        Vector3 direction = transform.position - target.position;
+        direction.Normalize();
+        
+        Vector3 velocity = new Vector3(-direction.y, direction.x) * speed;
+
+        transform.position += velocity * Time.deltaTime;
     }
 }
